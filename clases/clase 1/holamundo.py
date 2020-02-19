f82fba3428c1babba30f6da31a12809aa91cb494
““lesly <lesly2321@hotmail.com>
2020-02-19 12:11:39 -0500

    hola mundo

---
+++ clases/clase 1/holamundo.py
@@ -0,0 +1,19 @@
+#codigo primera clase
+nombre = "valeria"
+print (nombre)
+print (type(nombre))
+edad = 19
+print (edad)
+print (type(edad))
+dinero = 120.0
+print (dinero)
+print (type(dinero))
+nombre = lego
+print (nombre)
+print (type(nombre))
+tiempo de la empresa = 71
+print (tiempo de la empresa)
+print (type(tiempo de la empresa))
+ganancia = 358.000.000.000
+print (ganancia)
+print (type(ganancia))
